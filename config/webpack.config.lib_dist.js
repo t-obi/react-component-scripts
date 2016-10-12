957481dc9adefe43aac6093811872a22b3f41239
Tobias Kloht <tobias.kloht@gmail.com>
2016-10-12 04:17:55 +0200

    add webpack config for lib build

---
+++ config/webpack.config.lib_dist.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const paths = require('./paths');
+
+const PUBLIC_PATH = '/react-video-cover';
+const libraryName = require(paths.appPackageJson).name;
+
+const CONFIG_LIB_DIST = {
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  entry: [
+    paths.componentIndexJs,
+  ],
+  externals: {
+    'react': 'react',
+    'react-dom': 'react-dom'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        query: {
+          presets: ['stage-0', 'es2015', 'react'],
+          plugins: [
+            'transform-class-properties',
+          ],
+        },
+      },
+    ],
+  },
+  output: {
+    path: paths.componentDist,
+    filename: 'index.js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      CONFIG: {
+        basePath: JSON.stringify(PUBLIC_PATH),
+      },
+    }),
+    new webpack.NoErrorsPlugin(),
+  ],
+
+};
+
+module.exports = CONFIG_LIB_DIST;
